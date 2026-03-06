9fdfb1635ec4a33bbdab5058fa3d308015a93e54
Dylan Curzon <blueberry@curz.sh>
2026-03-06 08:31:03 +0000

    bench-identity: implement decoder-encoder for SignedChainAction

--- bench-identity/src/lib.rs
+++ bench-identity/src/lib.rs
@@ -27,7 +27,7 @@ use err_derive::Error;
 
 // protobuf: Wire protocol.
 extern crate protobuf;
-use protobuf::{EnumOrUnknown, Message};
+use protobuf::{EnumOrUnknown, Message, MessageField};
 
 extern crate rand;
 //use rand::rngs::SysRng;
@@ -45,8 +45,10 @@ use ed25519_dalek::{
     SigningKey,
     Verifier,
     VerifyingKey,
-    ed25519::signature::SignerMut
+    ed25519::signature::Signer
 };
+
+use crate::wire::SignedChainAction;
 
 fn fingerprint(key: &[u8; PUBLIC_KEY_LENGTH]) -> String {
     let hash = key.digest();
@@ -69,6 +71,8 @@ pub enum ParserError {
     BadIdentity(String),
     #[error(display = "an signature from {:?} cannot be verified", _0)]
     BadSignature(String),
+    #[error(display = "cannot write object to binary format")]
+    BadObject(),
     #[error(display = "a binary payload cannot be parsed")]
     BinaryFormatError(),
 }
@@ -80,7 +84,7 @@ struct Identity {
 }
 
 trait Sign {
-    fn sign(&mut self, bytes: &[u8]) -> [u8; SIGNATURE_LENGTH];
+    fn sign(&self, bytes: &[u8]) -> [u8; SIGNATURE_LENGTH];
 }
 
 trait Verify {
@@ -93,7 +97,7 @@ struct ChainKey {
 }
 
 impl Sign for ChainKey {
-    fn sign(&mut self, bytes: &[u8]) -> [u8; SIGNATURE_LENGTH] {
+    fn sign(&self, bytes: &[u8]) -> [u8; SIGNATURE_LENGTH] {
         let signature = self.private_key.sign(bytes);
         signature.to_bytes()
     }
@@ -118,6 +122,7 @@ struct ChainAction {
     device_origin: Identity,
     action_type: ChainActionType,
     prev_signature: Vec<u8>,
+    //TODO payload
     __src: Option<wire::SignedChainAction>,
 }
 
@@ -136,27 +141,41 @@ struct UserIdentity {
     __src: Option<wire::UserIdentity>,
 }
 
-struct Parser {
-    chain_key: ChainKey,
-}
+struct Parser {}
 
-trait Wire<T, W>
+trait Encoder<W>
 where W: Message {
-    fn to_wire_type(&mut self, _: &T) -> Result<W, ParserError>;
-    fn from_wire_type(&mut self, _: &W) -> Result<T, ParserError>;
+    fn encode(&self) -> Result<W, ParserError>;
 }
 
-impl Wire<Identity, wire::Identity> for Parser {
-    fn to_wire_type(&mut self, id: &Identity) -> Result<wire::Identity, ParserError> {
+trait SignerEncoder<W, K>
+where W: Message, K: Sign {
+    fn encode(&self, _: &K) -> Result<W, ParserError>;
+}
+
+trait Decoder<T>
+where Self: Message {
+    fn decode(&self) -> Result<T, ParserError>;
+}
+
+trait VerifierDecoder<T>
+where Self: Message {
+    fn decode(&self, _: Identity) -> Result<T, ParserError>;
+}
+
+impl Encoder<wire::Identity> for Identity {
+    fn encode(&self) -> Result<wire::Identity, ParserError> {
         let mut w = wire::Identity::new();
-        w.public_key = Some(id.public_key.to_vec());
-        w.public_key_type = Some(EnumOrUnknown::new(id.public_key_type));
+        w.public_key = Some(self.public_key.to_vec());
+        w.public_key_type = Some(EnumOrUnknown::new(self.public_key_type));
         Ok(w)
     }
+}
 
-    fn from_wire_type(&mut self, wire: &wire::Identity) -> Result<Identity, ParserError> {
+impl Decoder<Identity> for wire::Identity {
+    fn decode(&self) -> Result<Identity, ParserError> {
         // check field presence: public_key.
-        let public_key: Vec<u8> = wire.public_key.clone()
+        let public_key: Vec<u8> = self.public_key.clone()
             .ok_or(ParserError::MissingRequiredField("public_key"))?;
         // check field size: public_key.
         let public_key: &[u8; PUBLIC_KEY_LENGTH] = public_key.as_array::<PUBLIC_KEY_LENGTH>()
@@ -166,68 +185,88 @@ impl Wire<Identity, wire::Identity> for Parser {
                 present: public_key.len(),
             })?;
         // check field presence: public_key_type.
-        let public_key_type = wire.public_key_type
+        let public_key_type = self.public_key_type
             .and_then(|k| k.enum_value().ok())
             .ok_or(ParserError::MissingRequiredField("public_key_type"))?;
         let result = Identity {
             public_key: *public_key,
             public_key_type,
-            __src: Some(wire.clone()),
+            __src: Some(self.clone()),
         };
         Ok(result)
     }
 }
 
-impl Wire<ChainAction, wire::SignedChainAction> for Parser {
-    fn to_wire_type(&mut self, act: &ChainAction) -> Result<wire::SignedChainAction, ParserError> {
-        let mut unsigned = wire::ChainAction::new();
-        unsigned.counter        = Some(act.counter);
-        unsigned.created_at     = Some(act.created_at);
-        unsigned.device_origin  = self.to_wire_type(&act.device_origin).ok().into();
-        unsigned.action_type    = Some(EnumOrUnknown::new(act.action_type));
-        unsigned.prev_signature = Some(act.prev_signature.to_vec());
-        let mut signed = wire::SignedChainAction::new();
-        let bytes = unsigned.write_to_bytes().unwrap();
-        signed.chain_action_signature = Some(self.chain_key.sign(&bytes).to_vec());
-        signed.chain_action_bytes     = Some(bytes); 
-        Ok(signed)
-    }
+impl Encoder<wire::ChainAction> for ChainAction {
+    fn encode(&self) -> Result<wire::ChainAction, ParserError> {
+        let device_origin: wire::Identity = self.device_origin
+            .encode()
+            .map_err(|_| ParserError::BadObject())?;
 
-    fn from_wire_type(&mut self, signed: &wire::SignedChainAction) -> Result<ChainAction, ParserError> {
-        // parse bytes
-        let bytes = signed.chain_action_bytes.as_ref()
-            .ok_or(ParserError::MissingRequiredField("chain_action_bytes"))?;
-        let action = wire::ChainAction::parse_from_bytes(&bytes).ok()
-            .ok_or(ParserError::BinaryFormatError())?;
+        let mut unsigned = wire::ChainAction::new();
+        unsigned.counter = Some(self.counter);
+        unsigned.created_at = Some(self.created_at);
+        unsigned.device_origin = Some(device_origin).into();
+        unsigned.action_type = Some(EnumOrUnknown::new(self.action_type));
+        unsigned.prev_signature = Some(self.prev_signature.to_vec());
+        //TODO payload
+        Ok(unsigned)
+    }
+}
+
+impl Decoder<ChainAction> for wire::ChainAction {
+    fn decode(&self) -> Result<ChainAction, ParserError> {
+        //TODO: this will be done in UserIdentity decoding as a VerifierDecoder
+        //let bytes = signed.chain_action_bytes.as_ref()
+        //    .ok_or(ParserError::MissingRequiredField("chain_action_bytes"))?;
+        //let action = wire::ChainAction::parse_from_bytes(&bytes).ok()
+        //    .ok_or(ParserError::BinaryFormatError())?;
         // get signature
-        let signature = signed.chain_action_signature.as_ref()
-            .ok_or(ParserError::MissingRequiredField("chain_action_signature"))?;
-        let signature = signature
-            .as_array::<SIGNATURE_LENGTH>()
-            .ok_or(ParserError::WronglySizedSignature(signature.len()))?;
+        //let signature = signed.chain_action_signature.as_ref()
+        //    .ok_or(ParserError::MissingRequiredField("chain_action_signature"))?;
+        //let signature = signature
+        //    .as_array::<SIGNATURE_LENGTH>()
+        //    .ok_or(ParserError::WronglySizedSignature(signature.len()))?;
         // verify signature
-        self.chain_key.identity.verify(&bytes, &signature)?;
-        let counter = action.counter
+        //self.chain_key.identity.verify(&bytes, &signature)?;
+        //
+        let counter = self.counter
             .ok_or(ParserError::MissingRequiredField("counter"))?;
-        let created_at = action.created_at
+        let created_at = self.created_at
             .ok_or(ParserError::MissingRequiredField("created_at"))?;
-        let device_origin = action.device_origin.as_ref()
-            .and_then(|d| self.from_wire_type(d).ok())
+        let device_origin = self.device_origin.as_ref()
             .ok_or(ParserError::MissingRequiredField("device_origin"))?;
-        let action_type = action.action_type
+        let device_origin = device_origin
+            .decode()
+            .ok()
+            .ok_or(ParserError::BinaryFormatError())?;
+        let action_type = self.action_type
             .and_then(|a| a.enum_value().ok())
-            .ok_or(ParserError::MissingRequiredField("action_type"))?;
-        let prev_signature = action.prev_signature.clone()
+            .ok_or(ParserError::MissingRequiredField("self_type"))?;
+        let prev_signature = self.prev_signature.clone()
             .ok_or(ParserError::MissingRequiredField("prev_signature"))?;
+        //TODO payload
         let result = ChainAction {
             counter,
             created_at,
             device_origin,
             action_type,
             prev_signature,
-            __src: Some(signed.clone()),
+            __src: None,
         };
         Ok(result)
+    }
+}
+
+impl SignerEncoder<wire::SignedChainAction, ChainKey> for ChainAction {
+    fn encode(&self, chain_key: &ChainKey) -> Result<wire::SignedChainAction, ParserError> {
+        let unsigned: wire::ChainAction = Encoder::encode(self)
+            .map_err(|_| ParserError::BadObject())?;
+        let mut signed = wire::SignedChainAction::new();
+        let bytes = unsigned.write_to_bytes().unwrap();
+        signed.chain_action_signature = Some(chain_key.sign(&bytes).to_vec());
+        signed.chain_action_bytes     = Some(bytes); 
+        Ok(signed)
     }
 }
 
